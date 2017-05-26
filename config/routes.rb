bd1792e8d38f3e8ff4d14e4e13c65cbd5364231b
eloi tribo <divamossat14@gmail.com>
2017-05-26 14:30:31 +0200

    como3vacas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'statics/index'
+  	get 'statics/index'
+	   get 'statics/about'
 
-  get 'statics/about'
+  	resources :peliculas
+  	resources :comments
+  
+  	get 'comentarios/index'
+  	get 'movies/index'
 
-  resources :peliculas
-  resources :comments
-  get 'comentarios/index'
 
-  get 'movies/index'
+	root :to => "statics#index"
+	get '/about' => 'static#about', :as => :about
+
+  devise_for :users
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
