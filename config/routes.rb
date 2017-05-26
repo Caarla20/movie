96b44398227c906b406e6888c639665364bd0a26
eloi tribo <divamossat14@gmail.com>
2017-05-26 10:50:44 +0200

    canvis 3

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
 	root :to => "statics#index"
 	get '/about' => 'statics#about', :as => :about
+	devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
