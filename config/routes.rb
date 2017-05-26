101bb6cf2aecba12600d7bf3a8107384d612e07b
eloi tribo <divamossat14@gmail.com>
2017-05-26 10:04:44 +0200

    Añadimos controlador static 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'statics/index'
+	get 'statics/index'
 
-  get 'statics/about'
+	get 'statics/about'
 
-  resources :peliculas
-  resources :comments
-  get 'comentarios/index'
+	resources :peliculas
+	resources :comments
+	get 'comentarios/index'
 
-  get 'movies/index'
+	get 'movies/index'
 
-root :to => "statics#index"
-get '/about => 'statics#about, :as => :about
+	root :to => "statics#index"
+	get '/about' => 'statics#about', :as => :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
